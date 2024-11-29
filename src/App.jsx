a17d80b577e9ef1e4c7829decae75babe8ec7472
Your Name <shaurya.com>
2024-11-29 18:27:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Hero from './components/hero'
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+        <Navbar />
+        <Hero />
+        <About />
+        <Features />
+        <Story />
+        <Contact />
+        <Footer />
+    </main>
+  )
+}
+
+export default App
